f52783bcedb78d73277c33187e337d482f3cf047
joaovitorhipolito <joaovitorhipolito23@gmail.com>
2024-01-15 21:26:07 -0300

    parte

--- src/main/index.ts
+++ src/main/index.ts
@@ -5,6 +5,7 @@ import { logger } from "@shared/helpers/logge.winston";
 import { Application } from "express";
 import { createExpressApplication } from "./presentation/http/app.express";
 import { error } from "console";
+import { createSPAExpressApplication } from './presentation/http/spa.express';
 
 async function bootstrap() {
   //Carrega variáveis de ambiente do arquivo .env
@@ -24,6 +25,14 @@ async function bootstrap() {
 
   httpServer.listen({ port: port }, async () => {
     logger.ok(`Servidor HTTP Pronto e Ouvindo em http://${host_name}:${port}`);
+  });
+
+  const spa: Application = await createSPAExpressApplication();
+  logger.ok(`SPA - Aplicação Express Instanciada e Configurada`);
+  const httpServerSPA = await createHTTPServer(spa);
+  logger.ok('SPA - Servidor HTTP para  Instanciado e Configurado');
+  httpServerSPA.listen({ port: 5400 }, async () => {
+      logger.ok(`SPA - Servidor HTTP Pronto e Ouvindo em http://${host_name}:5400`);
   });
 
   prisma.$connect().then(async () => {
